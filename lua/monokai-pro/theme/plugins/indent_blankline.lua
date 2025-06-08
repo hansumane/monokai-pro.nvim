921c3cf91d772baa862668f2426726fb04b5a89e
Creasure <yurxo@bk.ru>
2025-06-08 06:41:42 +0300

    add highlights for indent-blankline v3

--- lua/monokai-pro/theme/plugins/indent_blankline.lua
+++ lua/monokai-pro/theme/plugins/indent_blankline.lua
@@ -7,21 +7,22 @@ function M.get(c, config, _)
   local isContextPro = config.plugins.indent_blankline.context_highlight == "pro"
   local isContextStartUnderline = config.plugins.indent_blankline.context_start_underline
   return {
+    -- indent-blankline (ibl) v3
+    IblIndent = { fg = c.editorIndentGuide.background },
+    IblWhitespace = { fg = c.editor.background },
+    IblSpace = isContextPro and { fg = c.base.blue } or {
+      fg = c.editorIndentGuide.activeBackground,
+    },
+    -- indent-blankline v2
     IndentBlanklineChar = { fg = c.editorIndentGuide.background },
+    IndentBlanklineSpaceChar = { fg = c.editor.background },
+    IndentBlanklineSpaceCharBlankline = { fg = c.editor.background },
     IndentBlanklineContextChar = isContextPro and { fg = c.base.blue } or {
       fg = c.editorIndentGuide.activeBackground,
     },
-    IndentBlanklineContextStart = {
-      sp = isContextPro and c.base.blue or c.editorIndentGuide.activeBackground,
-      underline = isContextStartUnderline,
+    IndentBlanklineContextSpaceChar = isContextPro and { fg = c.base.blue } or {
+      fg = c.editorIndentGuide.activeBackground,
     },
-    IndentBlanklineSpaceChar = { fg = c.editor.background }, -- highlight the character Tab
-    IndentBlankLineIndent1 = { fg = c.base.red },
-    IndentBlankLineIndent2 = { fg = c.base.blue },
-    IndentBlankLineIndent3 = { fg = c.base.yellow },
-    IndentBlankLineIndent4 = { fg = c.base.green },
-    IndentBlankLineIndent5 = { fg = c.base.cyan },
-    IndentBlankLineIndent6 = { fg = c.base.magenta },
   }
 end
 
